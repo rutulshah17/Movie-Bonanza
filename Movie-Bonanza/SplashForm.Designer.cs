943c642aa5ceeaa7c35a62ece04f7076c385a681
rutulshah17 <rutulshah17@gmail.com>
2017-02-27 14:39:30 -0500

    Added Splash Form !

---
+++ Movie-Bonanza/SplashForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SelectForm.cs
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MovieBonanzaLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SplashFormTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // MovieBonanzaLabel
+            // 
+            this.MovieBonanzaLabel.AutoSize = true;
+            this.MovieBonanzaLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieBonanzaLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MovieBonanzaLabel.Location = new System.Drawing.Point(178, 85);
+            this.MovieBonanzaLabel.Name = "MovieBonanzaLabel";
+            this.MovieBonanzaLabel.Size = new System.Drawing.Size(317, 39);
+            this.MovieBonanzaLabel.TabIndex = 0;
+            this.MovieBonanzaLabel.Text = "MOVIE BONANZA";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(152, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Best place to stream your favourite movies";
+            // 
+            // SplashFormTimer
+            // 
+            this.SplashFormTimer.Enabled = true;
+            this.SplashFormTimer.Interval = 3000;
+            this.SplashFormTimer.Tick += new System.EventHandler(this.SplashFormTimer_Tick);
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(673, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MovieBonanzaLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SplashForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MovieBonanzaLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer SplashFormTimer;
+    }
+}
---
+++ Movie-Bonanza/SplashForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectForm.cs
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            SelectForm selectForm = new SelectForm();
+            selectForm.previousForm = this;
+            
+            this.SplashFormTimer.Enabled = false;
+            selectForm.Show();
+            this.Hide();
+        }
+    }
+}
