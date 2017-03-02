bc858c621c5fd742b0faa1f529b2caef83554749
rutulshah17 <rutulshah17@gmail.com>
2017-03-02 08:44:49 -0500

    Added Property Class !

---
+++ Movie-Bonanza/PropertiesClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SelectForm.cs
+{
+    class PropertiesClass
+    {
+        public static List<TextBox> MovieDetails { get; set; }
+
+        public static Image MovieImage { get; set; }
+
+    }
+}
